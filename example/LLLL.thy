36e473052e44727271304bcdc4ecfeb0f928f6f7
Mario Alvarez <mario.mcgilvray.alvarez@gmail.com>
2017-11-09 14:05:44 -0800

    basic hex input/output routines

--- example/LLLL.thy
+++ example/LLLL.thy
@@ -1,9 +1,120 @@
-theory LLLL2
+theory LLLL
   
-  imports Main "../ContractSem" "../RelationalSem" "../ProgramInAvl" "../Hoare/Hoare" "../lem/Evm" 
+  imports Main Nat "../ContractSem" "../RelationalSem" "../ProgramInAvl" "../Hoare/Hoare" "../lem/Evm" 
 begin
-  
-  (* LLLL, mark 2 *)
+
+value "CHR''0'' :: char"
+(* convenience routines for reading/printing hex as string *)
+value "List.hd (''abcd'' :: char list)"
+
+definition hexread1_dom :: "char \<Rightarrow> bool" where
+"hexread1_dom c = (c = CHR ''0'' \<or> c = CHR ''1'' \<or> c = CHR ''2'' \<or>
+                   c = CHR ''3'' \<or> c = CHR ''4'' \<or> c = CHR ''5'' \<or>
+                   c = CHR ''6'' \<or> c = CHR ''7'' \<or> c = CHR ''8'' \<or>
+                   c = CHR ''9'' \<or> c = CHR ''A'' \<or> c = CHR ''B'' \<or>
+                   c = CHR ''D'' \<or> c = CHR ''E'' \<or> c = CHR ''F'')"
+
+definition hexread1 :: "char \<Rightarrow> nat" where
+"hexread1 c = (if c = (CHR ''0'') then 0 else
+               if c = (CHR ''1'') then 1 else
+               if c = (CHR ''2'') then 2 else
+               if c = (CHR ''3'') then 3 else
+               if c = (CHR ''4'') then 4 else
+               if c = (CHR ''5'') then 5 else
+               if c = (CHR ''6'') then 6 else
+               if c = (CHR ''7'') then 7 else
+               if c = (CHR ''8'') then 8 else
+               if c = (CHR ''9'') then 9 else
+               if c = (CHR ''A'') then 10 else
+               if c = (CHR ''B'') then 11 else
+               if c = (CHR ''C'') then 12 else
+               if c = (CHR ''D'') then 13 else
+               if c = (CHR ''E'') then 14 else
+               if c = (CHR ''F'') then 15 else
+               undefined)"
+
+definition hexwrite1_dom :: "nat \<Rightarrow> bool" where
+"hexwrite1_dom n = (n < 16)"
+
+definition hexwrite1 :: "nat \<Rightarrow> char" where
+"hexwrite1 c = (if c = 0 then CHR ''0'' else
+                if c = 1 then CHR ''1'' else
+                if c = 2 then CHR ''2'' else
+                if c = 3 then CHR ''3'' else
+                if c = 4 then CHR ''4'' else
+                if c = 5 then CHR ''5'' else
+                if c = 6 then CHR ''6'' else
+                if c = 7 then CHR ''7'' else
+                if c = 8 then CHR ''8'' else
+                if c = 9 then CHR ''9'' else
+                if c = 10 then CHR ''A'' else
+                if c = 11 then CHR ''B'' else
+                if c = 12 then CHR ''C'' else
+                if c = 13 then CHR ''D'' else
+                if c = 14 then CHR ''E'' else
+                if c = 15 then CHR ''F'' else undefined)"
+
+value "(1 < (0::nat))"
+
+lemma hexread1_hexwrite1 : "hexread1_dom c \<Longrightarrow> hexwrite1 (hexread1 c) = c"
+  apply (auto simp add:hexread1_dom_def hexread1_def hexwrite1_def)
+  done
+
+lemma hexwrite1_help :
+"n < 16 \<Longrightarrow>
+    n \<noteq> 15 \<Longrightarrow>
+    n \<noteq> 14 \<Longrightarrow>
+    n \<noteq> 13 \<Longrightarrow>
+    n \<noteq> 12 \<Longrightarrow>
+    n \<noteq> 11 \<Longrightarrow>
+    n \<noteq> 10 \<Longrightarrow>
+    n \<noteq> 9 \<Longrightarrow>
+    n \<noteq> 8 \<Longrightarrow>
+    n \<noteq> 7 \<Longrightarrow>
+    n \<noteq> 6 \<Longrightarrow>
+    n \<noteq> 5 \<Longrightarrow>
+    n \<noteq> 4 \<Longrightarrow>
+    n \<noteq> 3 \<Longrightarrow>
+    n \<noteq> 2 \<Longrightarrow>
+    n \<noteq> Suc 0 \<Longrightarrow>
+    0 < n \<Longrightarrow> False"
+proof(induction n, auto)
+qed 
+
+lemma hexwrite1_hexread1 : "hexwrite1_dom n \<Longrightarrow> hexread1 (hexwrite1 n) = n"
+  apply(auto simp add:hexwrite1_dom_def hexwrite1_def)
+                  apply(auto simp add:hexread1_def)
+                  apply(insert hexwrite1_help, auto)
+  done
+
+
+definition hexread2 :: "char \<Rightarrow> char \<Rightarrow> nat" where
+"hexread2 c1 c2 = (16 * (hexread1 c1) + hexread1 c2)"
+
+(* we need to reverse the input list? *)
+(* TODO: later handle zero padding for odd numbers of bytes *)
+fun hexread' :: "char list \<Rightarrow> nat \<Rightarrow> nat" where
+"hexread' [] n = n"
+| "hexread' [_] _ = undefined"
+| "hexread' (n1#n2#t) a = hexread' t (hexread2 n1 n2 + 256 * a)"
+
+definition hexread :: "char list \<Rightarrow> nat" where
+"hexread ls = hexread' ls 0"
+
+fun hexwrite2 :: "8 word \<Rightarrow> (char * char)" where
+"hexwrite2 w = 
+  (case Divides.divmod_nat (Word.unat w) 16 of
+       (d,m) \<Rightarrow> (hexwrite1 d, hexwrite1 m))"
+
+(* TODO: make sure we aren't supposed to do the reverse of this *)
+fun hexwrite :: "8 word list \<Rightarrow> char list" where
+"hexwrite [] = []"
+| "hexwrite (h#t) = (case hexwrite2 h of
+                     (c1, c2) \<Rightarrow> c1#c2#(hexwrite t))"
+
+value "(hexwrite [1,2])"
+
+(* *)
 
 (* we need to rule out invalid, PC, and misc instrs *)
 (* stack manipulation should be OK *)
@@ -516,6 +627,9 @@ datatype consume_label_result =
   | CNone "ll3 list"
   | CFail
 
+(* this prevents multiple locations for the same label name
+   because it will only "consume" one label per name
+   and then it will fail later on the other one *)
 (* subroutine for assign_label, marks label as consumed *)
 fun ll3_consume_label :: "childpath \<Rightarrow> nat  \<Rightarrow> ll3 list \<Rightarrow> consume_label_result" where
  "ll3_consume_label p n [] = CNone []"
@@ -886,7 +1000,7 @@ value "(case prog3 of
 value "Word.word_of_int 1 :: 1 word"
 value "Evm.byteFromNat 255"
 
-value "Divides.divmod_nat 255 255"
+value "Divides.divmod_nat 255 256"
 
 (* TODO ensure these are coming out in the right order *)
 (* TODO this terminates because divmod decreases number *)
