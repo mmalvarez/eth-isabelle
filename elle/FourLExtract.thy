f8519037ca2eaed5bd8108a140e09c41e6f3cdd1
Mario Alvarez <mario.mcgilvray.alvarez@gmail.com>
2018-04-13 13:28:36 -0700

    working OCaml extraction of compiler

--- elle/FourLExtract.thy
+++ elle/FourLExtract.thy
@@ -1,13 +1,13 @@
 theory FourLExtract
-  imports Main ElleSyntax ElleCompiler FourL ElleUtils
+  imports Main ElleSyntax ElleCompiler FourL ElleUtils String "~~/src/HOL/Library/Code_Char"
 begin
 
 definition compilerFuel where "compilerFuel = 100"
 
 value "hexwrite [Evm.byteFromNat 125]"
 
-definition compiler :: "string \<Rightarrow> string option" where
-"compiler s =
+definition compiler_string :: "string \<Rightarrow> string option" where
+"compiler_string s =
   (case llll_parse_complete s of
    None \<Rightarrow> None
   | Some l4 \<Rightarrow>
@@ -16,11 +16,33 @@ definition compiler :: "string \<Rightarrow> string option" where
       | Some wl \<Rightarrow> Some (hexwrite wl)
    ))"
 
-export_code compiler in SML
+definition compiler :: "String.literal \<Rightarrow> String.literal option" where
+"compiler l =
+  (case compiler_string (String.literal.explode l) of
+   None \<Rightarrow> None
+  | Some s \<Rightarrow> Some (String.implode s))"
+
+export_code String.literal.explode
+
+(*declare equal_literal_def [code del]*)
+value "String.implode ''a''"
+
+value "compiler (String.implode ''a'')"
+(*
+export_code  compiler in SML
 module_name FourL file "./generated/FourL.sml"
+*)
+(* need a "code-printing" tweak
+- use OCaml strings, not char lists
+- use OCaml ints, not strs *)
+
+
 
 export_code compiler in OCaml
 module_name FourL file "./generated/FourL.ml"
-
+(*
+export_code compiler in Haskell
+module_name FourL file "./generated/"
+*)
 
 end
--- elle/FourLShim.ml
+++ elle/FourLShim.ml
@@ -1,49 +1,20 @@
-open FourL;;
+(* from RosettaCode *)
+let load_file f =
+	let ic = open_in f in
+	let n = in_channel_length ic in
+	let s = String.create n in
+	really_input ic s 0 n;
+	close_in ic;
+	(s);;
 
-let rec isaNumToOcamlNum n =
-match n with
-One -> 1
-| Bit0 n' -> 2 * isaNumToOcamlNum n'
-| Bit1 n' -> 2 * isaNumToOcamlNum n' + 1
-
-let isaCharToOcamlChar c =
-match c with
-Zero_char -> Char.chr 0
-| Char n -> Char.chr (isaNumToOcamlNum n)
-
-let isaStrToOcamlCharList s = List.map isaCharToOcamlChar s
-
-(* from stackoverflow *)
-let ocamlCharListToOcamlStr cl = String.concat "" (List.map (String.make 1) cl)
-
-let isaStrToOcamlStr s = isaStrToOcamlCharList (ocamlCharListToOcamlStr s)
-
-(* from stackoverflow *)
-let ocamlStrToOcamlCharList s =
-  let rec exp i l =
-    if i < 0 then l else exp (i - 1) (s.[i] :: l) in
-  exp (String.length s - 1) []
-
-let rec ocamlNumToIsaNum n =
-if n = 0 || n = 1 then One (* n = 0 should not happen *)
-else if n % 2 = 0 then Bit0 (ocamlNumToIsaNum (n/2))
-else Bit1 (ocamlNumToIsaNum ((n - 1)/2))
-
-let ocamlCharToIsaChar n =
-    match (Char.code n) with
-    0 -> Zero_char
-    | _ -> Char (ocamlNumToIsaNum n)
-
-let ocamlCharListToIsaCharList s = List.map ocamlCharToIsaChar s
-
-let ocamlStrToIsaCharList s =
-  ocamlCharListToIsaCharList (ocamlStrToOcamlCharList s)
-
-(* remaining to do - argument parsing*)
-
+(* first argv argument is the file name.
+TODO: handle parsing of options later, probably in Isabelle *)
 let main =
-  match (compiler (explode "()")) with
-    None -> print_string "nope\n"
-   | Some s -> print_string "yep\n"
-
-(* Idea: we want to *)
+	if Array.length Sys.argv > 1 then
+		let content = load_file (Sys.argv. (1)) in
+	        let out = FourL.FourL.compiler content in
+	        (match out with
+	         None -> prerr_endline "ERROR: compilation error"
+	         | Some outs -> print_endline outs
+                )
+	else prerr_endline "ERROR: no filename specified";;
